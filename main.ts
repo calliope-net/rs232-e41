c248c87f6c47df79dccb5e75ea030d95ae65abf8
calliope-net <141074597+calliope-net@users.noreply.github.com>
2025-02-15 18:38:13 +0100

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,14 @@
+function fb_Licht_an () {
+    return pins.analogReadPin(AnalogPin.P2) < 150
+}
+input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
+    basic.showNumber(pins.analogReadPin(AnalogPin.P2))
+})
+lcd16x2rgb.initLCD(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2_x3E))
+loops.everyInterval(100, function () {
+    if (fb_Licht_an()) {
+        basic.showNumber(1)
+    } else {
+        basic.showNumber(0)
+    }
+})
